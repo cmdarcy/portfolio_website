5dd400e46ecc7b2295f2333ac52cd3aa1d02bb0f
Christopher d'Arcy <cdarcy656481@gmail.com>
2023-07-24 20:13:11 -0400

    add label descriptors for skills logo

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+const skillsLogos = document.querySelectorAll('[data-skills-logo]')
+
+skillsLogos.forEach(logo => {
+    logo.addEventListener('mouseenter', (ev) => {
+        console.log(`You moused into the ${ev.target.querySelector('span')} logo!`);
+        let label = ev.target.querySelector('span');
+        if (label) {
+            ev.target.querySelector('span').style.display = 'block';
+        }
+    });
+    logo.addEventListener('mouseleave', (ev) => {
+        console.log(`You moused out of the ${ev.target.querySelector('span')} logo!`);
+        let label = ev.target.querySelector('span');
+        if (label) {
+            ev.target.querySelector('span').style.display = 'none';
+        }
+    });
+})
